c24bec5e3dc3980baa5d544e4452acab72c6f816
Radovan Polakovic <r.polakovic@arenae.ch>
2013-02-18 09:44:39 +0100

    Revert "edit perspective"
    This reverts commit 0a53dc2f09e87857c69687b9b73db83e650bd7ba.

--- TextEditorForChildren/src/texteditorforchildren/Perspective.java
+++ TextEditorForChildren/src/texteditorforchildren/Perspective.java
@@ -6,6 +6,5 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
-		layout.setEditorAreaVisible(true);
 	}
 }
