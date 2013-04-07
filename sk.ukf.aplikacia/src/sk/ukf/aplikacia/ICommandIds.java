2eb26e4bc990c650bc1fa7d2d9aaf25bdb7b4b19
Radovan Polakovic <r.polakovic@arenae.ch>
2013-04-07 21:06:55 +0200

    mail template application

---
+++ sk.ukf.aplikacia/src/sk/ukf/aplikacia/ICommandIds.java
@@ -0,0 +1,15 @@
+package sk.ukf.aplikacia;
+
+/**
+ * Interface defining the application's command IDs.
+ * Key bindings can be defined for specific commands.
+ * To associate an action with a command, use IAction.setActionDefinitionId(commandId).
+ *
+ * @see org.eclipse.jface.action.IAction#setActionDefinitionId(String)
+ */
+public interface ICommandIds {
+
+    public static final String CMD_OPEN = "sk.ukf.aplikacia.open";
+    public static final String CMD_OPEN_MESSAGE = "sk.ukf.aplikacia.openMessage";
+    
+}
