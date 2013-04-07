ee9209db782c8d6ce45f1f348d546fd210cdb535
Radovan Polakovic <r.polakovic@arenae.ch>
2013-04-07 21:06:55 +0200

    mail template

---
+++ mail.template/src/a/ICommandIds.java
@@ -0,0 +1,15 @@
+package a;
+
+/**
+ * Interface defining the application's command IDs.
+ * Key bindings can be defined for specific commands.
+ * To associate an action with a command, use IAction.setActionDefinitionId(commandId).
+ *
+ * @see org.eclipse.jface.action.IAction#setActionDefinitionId(String)
+ */
+public interface ICommandIds {
+
+    public static final String CMD_OPEN = "a.open";
+    public static final String CMD_OPEN_MESSAGE = "a.openMessage";
+    
+}
